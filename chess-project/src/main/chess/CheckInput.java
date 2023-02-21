f97f47507b950dbc300f956b457717f60c0b3d92
r67083sr <sourabh.roy@student.manchester.ac.uk>
2023-02-21 21:55:40 +0000

    input validation updated

--- chess-project/src/main/chess/CheckInput.java
+++ chess-project/src/main/chess/CheckInput.java
@@ -3,6 +3,19 @@ package chess;
 public class CheckInput {
 	
 	public static boolean checkCoordinateValidity(String input){
+		if(input.length()!=2)
+		return false;
+		else
+		{
+			String row ="abcdefgh" ;
+			String col ="12345678" ;
+			char i_value = input.charAt(0);
+			char j_value = input.charAt(1);
+			if(row.indexOf(j_value)==-1)
+			return false;
+			if(col.indexOf(i_value)==-1)
+			return false;
+		}
 		return true;
 	}
 }
