54e06b6f471560ad616d4ff0f3b12a91583a9d5d
r67083sr <sourabh.roy@student.manchester.ac.uk>
2023-02-22 13:33:07 +0000

    Knight moves simplified

--- chess-project/src/main/chess/Knight.java
+++ chess-project/src/main/chess/Knight.java
@@ -1,5 +1,6 @@
 package chess;
 
+
 class Knight extends Piece{
 	private PieceColour colour;
 	private String symbol;
@@ -23,12 +24,9 @@ class Knight extends Piece{
 	}
 
 	@Override
-	public boolean isLegitMove(int i0, int j0, int i1, int j1) {
-		if(i0==i1 && j0==j1)
-		return false;
-		if((int)Math.abs(i0 -  i1)>2 || (int)Math.abs(j0-j1)>2)
-		return false;
-		else if(((int)Math.abs(i0 -  i1)==2 && (int)Math.abs(j0-j1)==1))
+	public boolean isLegitMove(int i0, int j0, int i1, int j1) 
+	{
+		 if(((int)Math.abs(i0 - i1)==2 && (int)Math.abs(j0-j1)==1))
 		{
 			if(Board.hasPiece(i1, j1))
 			{
@@ -36,9 +34,13 @@ class Knight extends Piece{
 					return false;
 				else if (Board.getPiece(i0, j0).getColour()==PieceColour.WHITE && Board.getPiece(i1, j1).getColour()==PieceColour.WHITE)
 					return false;
+				else
+					return true;
 			}
+			else 
+			return true;
 		}
-		else if (((int)Math.abs(i0 -  i1)==1 && (int)Math.abs(j0-j1)==2))
+		else if (((int)Math.abs(i0 - i1)==1 && (int)Math.abs(j0-j1)==2))
 		{
 			if(Board.hasPiece(i1, j1))
 			{
@@ -46,10 +48,12 @@ class Knight extends Piece{
 					return false;
 				else if (Board.getPiece(i0, j0).getColour()==PieceColour.WHITE && Board.getPiece(i1, j1).getColour()==PieceColour.WHITE)
 					return false;
+				else
+					return true;
 			}
+			else
+			return true;
 		}
-		else if (((int)Math.abs(i0 -  i1)==2 && (int)Math.abs(j0-j1)==2))
 		return false;
-		return true;
 	}
 }
