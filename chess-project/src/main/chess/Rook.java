363dd601119f30fc3fc2d028c088c7d4ceb5a76e
r67083sr <sourabh.roy@student.manchester.ac.uk>
2023-02-22 19:17:21 +0000

    Rook code simplified

--- chess-project/src/main/chess/Rook.java
+++ chess-project/src/main/chess/Rook.java
@@ -23,58 +23,29 @@ class Rook extends Piece{
 	}
 
 	public boolean isLegitMove(int i0, int j0, int i1, int j1) {
+		int i;
 		if(i0==i1 && j0==j1)
 		return false;
-		int f1 = 0;
-		int a = 0; int b = 0;
-		if((i0 != i1) && (j0 != j1))
+		if(i0==i1)
 		{
-			return false;
+			int dx = (j0<j1)?1:-1;
+			for(i= j0+dx;i!=j1;i+=dx)
+			{
+				if(Board.hasPiece(i0, i))
+				return false;
+			}
 		}
-		else
+		else if(j0==j1)
 		{
-			if(i0 == i1)
+			int dy = (i0<i1)?1:-1;
+			for(i = i0+dy; i!=i1 ; i+=dy)
 			{
-				int start = Math.min(j0,j1);
-				int end = Math.max(j0,j1);
-				for(int i = start; i<=end ;i++)
-				{
-					if(Board.hasPiece(i0, i))
-					{
-						f1++; 
-						a = i0;
-						b = i;
-					}
-				}
-			}
-			else if (j0 == j1)
-			{
-				int start = Math.min(i0,i1);
-				int end = Math.max(i0,i1);
-				for(int i = start; i<=end ;i++)
-				{
-					if(Board.hasPiece(i, j0))
-					{
-						f1++; 
-						a = i;
-						b = j0;
-					}
-				}
-			}
-			if(f1 > 1)
-			return false;
-			else if(f1 == 1 && (a != i1 && b != j1))
-			{
-				return false;
-			}
-			else if (f1 == 1 && (a == i1 && b == j1))
-			{
-				if(Board.getPiece(i0, j0).getColour()==PieceColour.BLACK && Board.getPiece(i1, j1).getColour()==PieceColour.BLACK)
-				return false;
-				else if (Board.getPiece(i0, j0).getColour()==PieceColour.WHITE && Board.getPiece(i1, j1).getColour()==PieceColour.WHITE)
+				if(Board.hasPiece(i, j0))
 				return false;
 			}
 		}
-		return true;
+		else
+		return false;
+		return Board.hasPiece(i1, j1)==false;
 	}
 }
